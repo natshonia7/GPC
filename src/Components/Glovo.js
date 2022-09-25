83222534898a30e3a11da56c7a4c8c3a5a5bf7d4
natia <nati.shonia37@gmail.com>
2022-09-25 18:19:22 +0400

    Create Glovo.js

---
+++ src/Components/Glovo.js
@@ -0,0 +1,13 @@
+import gpc from '../assets/photo/glovo.png'
+export default function Glovo() {
+  return (
+
+      <div className="s-2">
+      
+        <div className="glovo">
+          <img src={gpc} alt="" className='imagegpc' />
+        </div>
+    </div>
+
+  )
+}
